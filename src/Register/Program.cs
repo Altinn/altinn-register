8962805a7e570f899baaf060277b439cdc624b2b
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2022-07-28 10:59:48 +0200

    updated ai registration (#44)
    * updated ai registration
    
    * search & replace doesnt alway work
    
    * fixe casing

--- src/Register/Program.cs
+++ src/Register/Program.cs
@@ -17,7 +17,7 @@ using Altinn.Platform.Register.Services.Interfaces;
 using Altinn.Platform.Telemetry;
 
 using AltinnCore.Authentication.JwtCookie;
-
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
@@ -43,7 +43,7 @@ ILogger logger;
 
 string vaultApplicationInsightsKey = "ApplicationInsights--InstrumentationKey";
 
-string applicationInsightsKey = string.Empty;
+string applicationInsightsConnectionString = string.Empty;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -107,7 +107,7 @@ async Task ConnectToKeyVaultAndSetApplicationInsights(ConfigurationManager confi
             SecretBundle secretBundle = await keyVaultClient
                 .GetSecretAsync(keyVaultSettings.SecretUri, vaultApplicationInsightsKey);
 
-            applicationInsightsKey = secretBundle.Value;
+            applicationInsightsConnectionString = string.Format("InstrumentationKey={0}", secretBundle.Value);
         }
         catch (Exception vaultException)
         {
@@ -137,15 +137,13 @@ void ConfigureLogging(ILoggingBuilder logging)
     // Clear log providers
     logging.ClearProviders();
 
-    // Setup up application insight if ApplicationInsightsKey is available
-    if (!string.IsNullOrEmpty(applicationInsightsKey))
+    // Setup up application insight if applicationInsightsConnectionString is available
+    if (!string.IsNullOrEmpty(applicationInsightsConnectionString))
     {
         // Add application insights https://docs.microsoft.com/en-us/azure/azure-monitor/app/ilogger
-        // Providing an instrumentation key here is required if you're using
-        // standalone package Microsoft.Extensions.Logging.ApplicationInsights
-        // or if you want to capture logs from early in the application startup 
-        // pipeline from Startup.cs or Program.cs itself.
-        logging.AddApplicationInsights(applicationInsightsKey);
+        logging.AddApplicationInsights(
+            configureTelemetryConfiguration: (config) => config.ConnectionString = applicationInsightsConnectionString,
+            configureApplicationInsightsLoggerOptions: (options) => { });
 
         // Optional: Apply filters to control what logs are sent to Application Insights.
         // The following configures LogLevel Information or above to be sent to
@@ -225,15 +223,19 @@ void ConfigureServices(IServiceCollection services, IConfiguration config)
     services.AddHttpClient<IParties, PartiesWrapper>();
     services.AddHttpClient<IAuthorization, AuthorizationWrapper>();
 
-    if (!string.IsNullOrEmpty(applicationInsightsKey))
+    if (!string.IsNullOrEmpty(applicationInsightsConnectionString))
     {
         services.AddSingleton(typeof(ITelemetryChannel), new ServerTelemetryChannel() { StorageFolder = "/tmp/logtelemetry" });
-        services.AddApplicationInsightsTelemetry(applicationInsightsKey);
+        services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+        {
+            ConnectionString = applicationInsightsConnectionString
+        });
+
         services.AddApplicationInsightsTelemetryProcessor<HealthTelemetryFilter>();
         services.AddApplicationInsightsTelemetryProcessor<IdentityTelemetryFilter>();
         services.AddSingleton<ITelemetryInitializer, CustomTelemetryInitializer>();
 
-        logger.LogInformation("Startup // ApplicationInsightsTelemetryKey = {ApplicationInsightsKey}", applicationInsightsKey);
+        logger.LogInformation("Startup // ApplicationInsightsTelemetryKey = {applicationInsightsConnectionString}", applicationInsightsConnectionString);
     }
 
     // Add Swagger support (Swashbuckle)
