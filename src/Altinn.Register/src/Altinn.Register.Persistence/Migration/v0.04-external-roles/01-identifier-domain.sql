cde299bdeb83afc1b7f7f7a8745f81ca2523d664
Aleksander Heintz <alxandr@alxandr.me>
2024-08-07 13:16:30 +0200

    fix: role name/description

--- src/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.04-external-roles/01-identifier-domain.sql
+++ src/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.04-external-roles/01-identifier-domain.sql
@@ -1,3 +1,3 @@
 -- Domain: register.identifier
-CREATE DOMAIN register.identifier AS text CONSTRAINT identifier_valid CHECK (value ~ '^[a-z][a-z0-9_]{4,29}$');
+CREATE DOMAIN register.identifier AS text CONSTRAINT identifier_valid CHECK (value ~ '^[a-z][a-z0-9_]{2,28}[a-z0-9]$');
 
--- src/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.04-external-roles/05-data-bedr-role.sql
+++ src/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.04-external-roles/05-data-bedr-role.sql
@@ -1,3 +1,3 @@
 INSERT INTO register.external_role_definition(source, identifier, name, description)
-  VALUES ('ccr'::register.party_source, 'bedr'::register.identifier, 'en=>"BEDR", nb=>"BEDR", nn=>"BEDR"'::register.translated_text, 'en=>"BEDR", nb=>"BEDR", nn=>"BEDR"'::register.translated_text);
+  VALUES ('ccr'::register.party_source, 'bedr'::register.identifier, 'en=>"Has as the registration entity", nb=>"Har som registreringsenhet", nn=>"Har som registreringseininga"'::register.translated_text, 'en=>"Has as the registration entity", nb=>"Har som registreringsenhet", nn=>"Har som registreringseininga"'::register.translated_text);
 
