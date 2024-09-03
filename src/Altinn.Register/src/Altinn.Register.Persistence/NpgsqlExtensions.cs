e80c3798f1768b8e0de389d8e5133aed046a0664
Aleksander Heintz <alxandr@alxandr.me>
2024-09-03 22:58:14 +0200

    feat: add npgsql mapping for address types (#288)
    * feat: add npgsql mapping for address types
    
    * chore: use serializeable theory data

--- src/Altinn.Register/src/Altinn.Register.Persistence/NpgsqlExtensions.cs
+++ src/Altinn.Register/src/Altinn.Register.Persistence/NpgsqlExtensions.cs
@@ -24,4 +24,28 @@ internal static class NpgsqlExtensions
         collection.Add(parameter);
         return parameter;
     }
+
+    /// <summary>
+    /// Gets a field value or a default value if the field is <see cref="DBNull.Value"/>.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="reader">The DB reader.</param>
+    /// <param name="ordinal">The column ordinal.</param>
+    /// <param name="defaultValue">Optional default value, defaults to <see langword="default"/>.</param>
+    /// <returns>The column value, or <paramref name="defaultValue"/> if the field is <see cref="DBNull.Value"/>.</returns>
+    public static T? GetFieldValueOrDefault<T>(this NpgsqlDataReader reader, int ordinal, T? defaultValue = default)
+        where T : notnull
+        => reader.IsDBNull(ordinal) ? defaultValue : reader.GetFieldValue<T>(ordinal);
+
+    /// <summary>
+    /// Gets a field value or a default value if the field is <see cref="DBNull.Value"/>.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="reader">The DB reader.</param>
+    /// <param name="name">The column name.</param>
+    /// <param name="defaultValue">Optional default value, defaults to <see langword="default"/>.</param>
+    /// <returns>The column value, or <paramref name="defaultValue"/> if the field is <see cref="DBNull.Value"/>.</returns>
+    public static T? GetFieldValueOrDefault<T>(this NpgsqlDataReader reader, string name, T? defaultValue = default)
+        where T : notnull
+        => GetFieldValueOrDefault(reader, reader.GetOrdinal(name), defaultValue);
 }
