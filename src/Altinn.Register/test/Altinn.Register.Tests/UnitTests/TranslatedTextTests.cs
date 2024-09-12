dfcb0dd433cec3cb09d0162319134bf7b7afdddd
Aleksander Heintz <alxandr@alxandr.me>
2024-09-12 12:16:36 +0200

    chore: add more tests

--- src/Altinn.Register/test/Altinn.Register.Tests/UnitTests/TranslatedTextTests.cs
+++ src/Altinn.Register/test/Altinn.Register.Tests/UnitTests/TranslatedTextTests.cs
@@ -48,6 +48,120 @@ public class TranslatedTextTests
     }
 
     [Fact]
+    public void Builder_Count_ReturnsCorrectCount()
+    {
+        var builder = TranslatedText.CreateBuilder();
+
+        builder.Should().HaveCount(0);
+
+        builder.Add(LangCode.En, "hello");
+        builder.Should().HaveCount(1);
+
+        builder.Add(LangCode.FromCode("fr"), "bonjour");
+        builder.Should().HaveCount(2);
+
+        builder.Add(LangCode.Nb, "hei");
+        builder.Should().HaveCount(3);
+
+        builder.Remove(LangCode.En);
+        builder.Should().HaveCount(2);
+
+        builder.Remove(LangCode.FromCode("fr"));
+        builder.Should().HaveCount(1);
+
+        builder.Remove(LangCode.Nb);
+        builder.Should().HaveCount(0);
+    }
+
+    [Fact]
+    public void Builder_Keys_ReturnsAllKeys()
+    {
+        var builder = TranslatedText.CreateBuilder();
+        var fr = LangCode.FromCode("fr");
+
+        builder.Should().HaveCount(0);
+        builder.ContainsKey(fr).Should().BeFalse();
+
+        builder.Add(LangCode.En, "hello");
+        builder.Keys.Should().Contain([LangCode.En]);
+        builder.ContainsKey(LangCode.En).Should().BeTrue();
+
+        builder.Add(fr, "bonjour");
+        builder.Keys.Should().Contain([LangCode.En, fr]);
+        builder.ContainsKey(fr).Should().BeTrue();
+
+        builder.Add(LangCode.Nb, "hei");
+        builder.Keys.Should().Contain([LangCode.En, fr, LangCode.Nb]);
+        builder.ContainsKey(LangCode.Nb).Should().BeTrue();
+
+        builder.Remove(LangCode.En);
+        builder.Keys.Should().Contain([fr, LangCode.Nb]);
+        builder.ContainsKey(LangCode.En).Should().BeFalse();
+
+        builder.Remove(fr);
+        builder.Keys.Should().Contain([LangCode.Nb]);
+        builder.ContainsKey(fr).Should().BeFalse();
+
+        builder.Remove(LangCode.Nb);
+        builder.Keys.Should().BeEmpty();
+        builder.ContainsKey(LangCode.Nb).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Builder_Values_ReturnsAllKeys()
+    {
+        var builder = TranslatedText.CreateBuilder();
+        var fr = LangCode.FromCode("fr");
+
+        builder.Should().HaveCount(0);
+
+        builder.Add(LangCode.En, "hello");
+        builder.Values.Should().Contain(["hello"]);
+
+        builder.Add(fr, "bonjour");
+        builder.Values.Should().Contain(["hello", "bonjour"]);
+
+        builder.Add(LangCode.Nb, "hei");
+        builder.Values.Should().Contain(["hello", "bonjour", "hei"]);
+
+        builder.Remove(LangCode.En);
+        builder.Values.Should().Contain(["bonjour", "hei"]);
+
+        builder.Remove(fr);
+        builder.Values.Should().Contain(["hei"]);
+
+        builder.Remove(LangCode.Nb);
+        builder.Values.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Builder_Indexer_ThrowsOnMissingKey()
+    {
+        var builder = TranslatedText.CreateBuilder();
+        
+        Assert.Throws<KeyNotFoundException>(() => builder[LangCode.En]);
+    }
+
+    [Fact]
+    public void Builder_Indexer_CanOverwrite()
+    {
+        var builder = TranslatedText.CreateBuilder();
+        var fr = LangCode.FromCode("fr");
+
+        builder[LangCode.En] = "1";
+        builder[LangCode.En].Should().Be("1");
+
+        builder[LangCode.En] = "2";
+        builder[LangCode.En].Should().Be("2");
+
+        builder[fr] = "3";
+        builder[fr].Should().Be("3");
+
+        builder[fr] = "4";
+        builder[fr].Should().Be("4");
+    }
+
+    [Fact]
     public void JsonRoundtrip()
     {
         var builder = TranslatedText.CreateBuilder();
