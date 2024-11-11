0b807a66bafe29e0d0de727d5072409a67671a43
Aleksander Heintz <alxandr@alxandr.me>
2024-11-11 10:01:44 +0100

    chore: add index on role-code

--- src/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.10-role-code/01-role-code.sql
+++ src/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.10-role-code/01-role-code.sql
@@ -10,6 +10,9 @@
 ALTER TABLE register.external_role_definition
   ADD COLUMN code text;
 
+CREATE UNIQUE INDEX uq_external_role_definition_code
+  ON register.external_role_definition (code);
+
 UPDATE
   register.external_role_definition
 SET
@@ -26,15 +29,12 @@ WHERE
   source = 'ccr'
   AND identifier = 'aafy';
 
-ALTER TABLE register.external_role_definition
-  ALTER COLUMN code SET NOT NULL;
-
 -- CREATE TABLE register.external_role_definition(
 --   source register.party_source NOT NULL,
 --   identifier register.identifier NOT NULL,
 --   name register.translated_text NOT NULL,
 --   description register.translated_text NOT NULL,
---   code text NOT NULL,
+--   code text,
 --   PRIMARY KEY (source, identifier)
 -- )
 -- TABLESPACE pg_default;
