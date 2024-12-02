c2af1be88503a44f144d8fa7298e29acd801b7d4
Aleksander Heintz <alxandr@alxandr.me>
2024-12-02 14:01:03 +0100

    chore: add support for jobs to wait for conditions

--- src/Altinn.Register/src/Altinn.Register/Jobs/JobRegistration.cs
+++ src/Altinn.Register/src/Altinn.Register/Jobs/JobRegistration.cs
@@ -8,8 +8,12 @@ namespace Altinn.Register.Jobs;
 public abstract class JobRegistration(
     string? leaseName,
     TimeSpan interval,
-    JobHostLifecycles runAt)
+    JobHostLifecycles runAt,
+    Func<IServiceProvider, CancellationToken, ValueTask>? waitForReady)
 {
+    private readonly object _lock = new();
+    private Task? _ready;
+
     /// <summary>
     /// Creates a new instance of the job.
     /// </summary>
@@ -31,4 +35,28 @@ public abstract class JobRegistration(
     /// Gets the <see cref="JobHostLifecycles"/> that the job should run at.
     /// </summary>
     public JobHostLifecycles RunAt => runAt;
+
+    /// <summary>
+    /// Waits for the job to be ready to run. This is only ran once per job registration.
+    /// </summary>
+    /// <param name="services">The service provider.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/>.</param>
+    /// <remarks>
+    /// Wait for ready runs in singleton scope, so it should not resolve scoped services.
+    /// </remarks>
+    public Task WaitForReady(IServiceProvider services, CancellationToken cancellationToken = default)
+    {
+        Task? task;
+        lock (_lock)
+        {
+            task = _ready;
+            if (task is null)
+            {
+                task = waitForReady is null ? Task.CompletedTask : waitForReady(services, cancellationToken).AsTask();
+                _ready = task;
+            }
+        }
+
+        return task.WaitAsync(cancellationToken);
+    }
 }
