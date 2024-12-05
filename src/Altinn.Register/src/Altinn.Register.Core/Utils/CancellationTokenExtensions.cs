1e41fdef9a60e3c0e7e60e55f813dea0d4d02ff8
Aleksander Heintz <alxandr@alxandr.me>
2024-12-05 11:17:50 +0100

    feat: add wait for cancellation token extension method

---
+++ src/Altinn.Register/src/Altinn.Register.Core/Utils/CancellationTokenExtensions.cs
@@ -0,0 +1,94 @@
+﻿#nullable enable
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Diagnostics;
+
+namespace Altinn.Register.Core.Utils;
+
+/// <summary>
+/// Extension methods for <see cref="CancellationToken"/>.
+/// </summary>
+public static class CancellationTokenExtensions
+{
+    /// <summary>
+    /// Returns a <see cref="CancellationTokenWaiter"/> that can be awaited to asynchronously wait for the <see cref="CancellationToken"/> to be canceled.
+    /// </summary>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+    /// <returns>A <see cref="CancellationTokenWaiter"/> that can be awaited.</returns>
+    public static CancellationTokenWaiter WaitForCancellationAsync(this CancellationToken cancellationToken)
+    {
+        return new CancellationTokenWaiter(cancellationToken);
+    }
+
+    /// <summary>
+    /// Awaits the <see cref="CancellationToken"/> to be canceled.
+    /// </summary>
+    public readonly struct CancellationTokenWaiter
+    {
+        private readonly CancellationToken _cancellationToken;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CancellationTokenWaiter"/> struct.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        internal CancellationTokenWaiter(CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+        }
+
+        /// <summary>
+        /// Gets an awaiter for this <see cref="CancellationTokenWaiter"/>.
+        /// </summary>
+        /// <returns></returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public CancellationTokenAwaiter GetAwaiter()
+        {
+            return new CancellationTokenAwaiter(_cancellationToken);
+        }
+    }
+
+    /// <summary>
+    /// Async/await awaiter for <see cref="CancellationToken"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public readonly struct CancellationTokenAwaiter
+        : INotifyCompletion
+        , ICriticalNotifyCompletion
+    {
+        private readonly CancellationToken _cancellationToken;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CancellationTokenAwaiter"/> struct.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        public CancellationTokenAwaiter(CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+        }
+
+        /// <summary>
+        /// Gets the result of the asynchronous wait operation.
+        /// </summary>
+        public void GetResult() 
+        {
+            if (!_cancellationToken.IsCancellationRequested)
+            {
+                ThrowHelper.ThrowInvalidOperationException("The awaiter was not awaited.");
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="CancellationToken"/> has been canceled.
+        /// </summary>
+        public bool IsCompleted => _cancellationToken.IsCancellationRequested;
+
+        /// <inheritdoc/>
+        public void OnCompleted(Action action)
+            => _cancellationToken.Register(action);
+
+        /// <inheritdoc/>
+        public void UnsafeOnCompleted(Action action)
+            => _cancellationToken.Register(action, useSynchronizationContext: false);
+    }
+}
---
+++ src/Altinn.Register/test/Altinn.Register.Tests/UnitTests/CancellationTokenExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿#nullable enable
+
+using Altinn.Register.Core.Utils;
+
+namespace Altinn.Register.Tests.UnitTests;
+
+public class CancellationTokenExtensionsTests
+{
+    [Fact]
+    public async Task WaitForCancellationAsync_NormalAsyncFlow()
+    {
+        using var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        var waiter = token.WaitForCancellationAsync();
+        var awaiter = waiter.GetAwaiter();
+
+        awaiter.IsCompleted.Should().BeFalse();
+
+        var callbacked = false;
+        awaiter.UnsafeOnCompleted(() => callbacked = true);
+
+        callbacked.Should().BeFalse();
+        await cts.CancelAsync();
+
+        callbacked.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task WaitForCancellationAsync_UnsafeOnCompleted_AfterCancellation_InvokesImmediately()
+    {
+        using var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        var waiter = token.WaitForCancellationAsync();
+        var awaiter = waiter.GetAwaiter();
+
+        awaiter.IsCompleted.Should().BeFalse();
+
+        await cts.CancelAsync();
+        var callbacked = false;
+        awaiter.UnsafeOnCompleted(() => callbacked = true);
+        callbacked.Should().BeTrue();
+    }
+
+    [Fact]
+
+    public async Task WaitForCancellationAsync_Await()
+    {
+        using var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        var task = Task.Run(async () => await token.WaitForCancellationAsync());
+        await task.WaitAsync(TimeSpan.FromMilliseconds(10)).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing | ConfigureAwaitOptions.ContinueOnCapturedContext);
+        task.IsCompleted.Should().BeFalse();
+
+        await cts.CancelAsync();
+        await task.WaitAsync(TimeSpan.FromMilliseconds(10));
+        task.IsCompleted.Should().BeTrue();
+    }
+}
