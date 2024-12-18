33d8c3cc1c7cb60c22a14613633e9e466713667a
Aleksander Heintz <alxandr@alxandr.me>
2024-12-18 09:42:21 +0100

    feat: consumer meters

--- src/Altinn.Register/src/Altinn.Register/PartyImport/A2PartyImportConsumer.cs
+++ src/Altinn.Register/src/Altinn.Register/PartyImport/A2PartyImportConsumer.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 
+using System.Diagnostics.Metrics;
 using Altinn.Authorization.ServiceDefaults.MassTransit;
+using Altinn.Register.Core;
 using Altinn.Register.Core.PartyImport.A2;
 using MassTransit;
 
@@ -14,14 +16,19 @@ public sealed class A2PartyImportConsumer
 {
     private readonly IA2PartyImportService _importService;
     private readonly ICommandSender _sender;
+    private readonly ImportMeters _meters;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="A2PartyImportConsumer"/> class.
     /// </summary>
-    public A2PartyImportConsumer(IA2PartyImportService importService, ICommandSender commandSender)
+    public A2PartyImportConsumer(
+        IA2PartyImportService importService,
+        ICommandSender commandSender,
+        RegisterTelemetry telemetry)
     {
         _importService = importService;
         _sender = commandSender;
+        _meters = telemetry.GetServiceMeters<ImportMeters>();
     }
 
     /// <inheritdoc />
@@ -29,5 +36,23 @@ public sealed class A2PartyImportConsumer
     {
         var party = await _importService.GetParty(context.Message.PartyUuid, context.CancellationToken);
         await _sender.Send(new UpsertPartyCommand { Party = party }, context.CancellationToken);
+        _meters.PartiesFetched.Add(1);
+    }
+
+    /// <summary>
+    /// Meters for <see cref="A2PartyImportConsumer"/>.
+    /// </summary>
+    private sealed class ImportMeters(RegisterTelemetry telemetry)
+        : IServiceMeters<ImportMeters>
+    {
+        /// <summary>
+        /// Gets a counter for the number of parties imported from A2.
+        /// </summary>
+        public Counter<int> PartiesFetched { get; }
+            = telemetry.CreateCounter<int>("register.party-import.a2.parties.fetched", "The number of parties fetched from A2.");
+
+        /// <inheritdoc/>
+        public static ImportMeters Create(RegisterTelemetry telemetry)
+            => new ImportMeters(telemetry);
     }
 }
