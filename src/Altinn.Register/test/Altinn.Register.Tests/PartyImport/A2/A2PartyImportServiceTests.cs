83a2bf8b1b2951ce12cd62267e0db0f7f30249e8
Aleksander Heintz <alxandr@alxandr.me>
2025-02-27 08:43:42 +0100

    chore: first name first (#420)

--- src/Altinn.Register/test/Altinn.Register.Tests/PartyImport/A2/A2PartyImportServiceTests.cs
+++ src/Altinn.Register/test/Altinn.Register.Tests/PartyImport/A2/A2PartyImportServiceTests.cs
@@ -96,10 +96,11 @@ public class A2PartyImportServiceTests
 
             var persRecord = partyRecord.Should().BeOfType<PersonRecord>().Which;
             persRecord.PersonIdentifier.Should().HaveValue().Which.Should().Be("25871999336");
-            persRecord.DisplayName.Should().HaveValue().Which.Should().Be("Nordmann Ola Bla");
+            persRecord.DisplayName.Should().HaveValue().Which.Should().Be("Ola Bla Nordmann");
             persRecord.FirstName.Should().HaveValue().Which.Should().Be("Ola");
             persRecord.MiddleName.Should().HaveValue().Which.Should().Be("Bla");
             persRecord.LastName.Should().HaveValue().Which.Should().Be("Nordmann");
+            persRecord.ShortName.Should().HaveValue().Which.Should().Be("Ola Bla Nordmann");
             persRecord.MailingAddress.Should().HaveValue().Which.Should().Be(new MailingAddress
             {
                 Address = "Blåbæreveien 7 8450 Stokmarknes",
