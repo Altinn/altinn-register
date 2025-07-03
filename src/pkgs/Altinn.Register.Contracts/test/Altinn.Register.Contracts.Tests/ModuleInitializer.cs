cfeb3d32fe209a7977affdf77803c8d8901b58fb
Aleksander Heintz <alxandr@alxandr.me>
2025-07-03 11:30:12 +0200

    test: contract serialization

---
+++ src/pkgs/Altinn.Register.Contracts/test/Altinn.Register.Contracts.Tests/ModuleInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Altinn.Register.Contracts.Tests;
+
+internal class ModuleInitializer
+{
+    private static int _initialized = 0;
+
+    [ModuleInitializer]
+    public static void Init()
+    {
+        if (Interlocked.Exchange(ref _initialized, 1) == 0)
+        {
+            VerifierSettings.UseSplitModeForUniqueDirectory();
+            UseProjectRelativeDirectory("Snapshots");
+        }
+    }
+}
