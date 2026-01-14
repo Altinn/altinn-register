76ea75f1acb1cdd56c83f483ac89483e8fba5dd9
Aleksander Heintz <alxandr@alxandr.me>
2026-01-14 10:32:28 +0100

    fix: integration-test db permissions

--- src/apps/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.44-better-case-insensitive/00-case-insensitive-collation.sql
+++ src/apps/Altinn.Register/src/Altinn.Register.Persistence/Migration/v0.44-better-case-insensitive/00-case-insensitive-collation.sql
@@ -1,4 +1,5 @@
 CREATE COLLATION case_insensitive (
     provider = icu, 
+    -- Use 'und' (undefined) locale with 'ks-level2' for case insensitivity
     locale = 'und-u-ks-level2', 
     deterministic = false);
