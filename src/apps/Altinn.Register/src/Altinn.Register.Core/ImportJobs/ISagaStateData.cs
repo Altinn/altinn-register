b017135991cf81310686a53274710416a6f44550
Aleksander Heintz <alxandr@alxandr.me>
2026-03-02 13:04:59 +0100

    chore: reduce saga data copying (#706)

--- src/apps/Altinn.Register/src/Altinn.Register.Core/ImportJobs/ISagaStateData.cs
+++ src/apps/Altinn.Register/src/Altinn.Register.Core/ImportJobs/ISagaStateData.cs
@@ -17,15 +17,20 @@ public interface ISagaStateData<TSelf>
     /// <summary>
     /// Read and convert the JSON to <typeparamref name="TSelf"/>.
     /// </summary>
-    /// <param name="reader">The <see cref="Utf8JsonReader"/> to read from.</param>
+    /// <param name="stream">The json <see cref="Stream"/>.</param>
     /// <param name="stateType">The state-type stored in the database.</param>
     /// <param name="options">The <see cref="JsonSerializerOptions"/> being used.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/>.</param>
     /// <returns>The value that was converted.</returns>
-    public static virtual TSelf? Read(ref Utf8JsonReader reader, string stateType, JsonSerializerOptions options)
+    public static virtual ValueTask<TSelf?> ReadAsync(
+        Stream stream,
+        string stateType,
+        JsonSerializerOptions options,
+        CancellationToken cancellationToken = default)
     {
         if (stateType == TSelf.StateType)
         {
-            return JsonSerializer.Deserialize<TSelf>(ref reader, options);
+            return JsonSerializer.DeserializeAsync<TSelf>(stream, options, cancellationToken);
         }
 
         return default;
