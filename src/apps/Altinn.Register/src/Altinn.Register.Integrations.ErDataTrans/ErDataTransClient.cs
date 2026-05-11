470cb5af7a18d836a06dbbec7cc008b9be53dcb8
tomshag <tom.sverre.hageland@digdir.no>
2026-05-11 14:26:30 +0200

    Løsning som henter filer fra SFTP mottak

---
+++ src/apps/Altinn.Register/src/Altinn.Register.Integrations.ErDataTrans/ErDataTransClient.cs
@@ -0,0 +1,139 @@
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+namespace Altinn.Register.Integrations.ErDataTrans;
+
+/// <summary>
+/// Client for retrieving SFTP files from BRG.
+/// </summary>
+internal sealed class ErDataTransClient
+{
+    private readonly string _username;
+    private readonly string _password;
+    private readonly string _host;
+    private readonly string _remotePath;
+    private readonly ISftpClient _client;
+
+    /// <summary>
+    /// Implementation of <see cref="ErDataTransClient"/>.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="ErDataTransClient"/> class.
+    /// </remarks>
+    /// <param name="remotePath">Remote path to access.</param>
+    /// <param name="host">Host to connect to.</param>
+    /// <param name="password">Password for authentication.</param>
+    /// <param name="user">Username for authentication.</param>
+    public ErDataTransClient(string user, string password, string host, string remotePath)
+    {
+        _username = user;
+        _password = password;
+        _host = host;
+        _remotePath = remotePath;
+        _client = new SftpClient(_host, _username, _password);
+    }
+
+    /// <summary>
+    /// Constructor for testing purposes, allows injection of a mock SftpClient.
+    /// </summary>
+    /// <param name="client">Test Client</param>
+    /// <param name="remotePath">Remote path to access.</param>
+    /// <param name="host">Host to connect to.</param>
+    /// <param name="password">Password for authentication.</param>
+    /// <param name="user">Username for authentication.</param>
+    public ErDataTransClient(string remotePath, string host, string password, string user, ISftpClient client)
+    {
+        _client = client;
+        _username = user;
+        _password = password;
+        _host = host;
+        _remotePath = remotePath;
+    }
+
+    /// <summary>
+    /// Retrieves new files from the SFTP server based on the last processed runId. It checks for files in the specified remote path, and if a file's runId is exactly one greater than the last processed runId, it downloads the file and marks it as downloaded by renaming it. The method returns a list of tuples containing the filename and its content as a stream.
+    /// </summary>
+    /// <param name="lastRunId">Last RunId</param>
+    /// <returns>List of tuples containing the filename and its content as a stream.</returns>
+    public List<Tuple<string, Stream>> GetNewFiles(int lastRunId = -1)
+    {
+        if (!_client.IsConnected)
+        {
+            _client.Connect();
+        }
+
+        int lastProcessedRunId = lastRunId;
+        IEnumerable<ISftpFile> files = _client.ListDirectory(_remotePath).Where(f => !f.IsDirectory && !f.IsSymbolicLink);
+        List<Tuple<string, Stream>> streams = new();
+        foreach (var file in files.OrderBy(f => f.Name))
+        {
+            if (file.Name.Contains("Downloaded"))
+            {
+                continue;
+            }
+
+            int runId = ErDataTransClient.GetRunIdFromFileName(file.Name);
+            if (!file.FullName.Contains("Downloaded")
+                && (lastRunId == -1 || runId == lastProcessedRunId + 1))
+            {
+                Stream stream = new MemoryStream();
+                _client.DownloadFile(file.FullName, stream);
+                stream.Position = 0; // Reset stream position after download
+                streams.Add(new Tuple<string, Stream>(file.Name, stream));
+                _client.RenameFile(file.FullName, file.FullName.Replace(".txt", "Downloaded.txt")); // Mark file as downloaded
+                lastProcessedRunId = runId;
+            }
+        }
+
+        _client.Disconnect();
+
+        return streams;
+    }
+
+    /// <summary>
+    /// Retrieves a specific file from the SFTP server based on the provided filename. It downloads the file content into a stream and returns it. After the download is complete, it disconnects from the SFTP server. The method assumes that the filename provided is the full path to the file on the SFTP server.
+    /// </summary>
+    /// <param name="filename">Filename to retrieve. Note: not the full path.</param>
+    /// <returns>Stream containing the file content.</returns>
+    public Stream GetSpecificFile(string filename)
+    {
+        if (!_client.IsConnected)
+        {
+            _client.Connect();
+        }
+
+        string fullPath = $"{_remotePath}/{filename}";
+        Stream stream = new MemoryStream();
+        _client.DownloadFile(fullPath, stream);
+        stream.Position = 0; // Reset stream position after download
+        _client.Disconnect();
+        return stream;
+    }
+
+    /// <summary>
+    /// Extracts the runId from the filename. Assumes filename format is always "bajXXXXX.txt" where XXXXX is the runId.
+    /// </summary>
+    /// <param name="filename">Filename to get runid for.</param>
+    /// <returns>Id of the running number of the file.</returns>
+    /// <exception cref="FormatException">Unable to retrieve runId from filename.</exception>
+    private static int GetRunIdFromFileName(string filename)
+    {
+        // Extract runId from filename, e.g., "baj05778.txt" -> 5778
+        int bajIndex = filename.IndexOf("baj");
+        int dotIndex = filename.IndexOf("Downloaded.txt");
+        if(dotIndex == -1)
+        {
+            dotIndex = filename.IndexOf(".txt");
+        }
+
+        string runIdPart = filename[(bajIndex + 3)..dotIndex]; // Assuming filename format is always "bajXXXXX.txt"
+        if (int.TryParse(runIdPart, out int runId))
+        {
+            return runId;
+        }
+        else
+        {
+            throw new FormatException($"Filename {filename} does not contain a valid runId.");
+        }
+    }
+}
---
+++ src/apps/Altinn.Register/test/Altinn.Register.Tests/UnitTests/ErDataTransClientTests.cs
@@ -0,0 +1,242 @@
+using System.Text;
+using Altinn.Register.Integrations.ErDataTrans;
+using Moq;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+namespace Altinn.Register.Tests.UnitTests;
+
+public class ErDataTransClientTests
+{
+    [Fact]
+    public void GetNewFiles_ReturnsNewFilesFromSftp()
+    {
+        // Arrange
+        var mockClient = new Mock<ISftpClient>();
+        var remotePath = "/remote/path";
+        string username = "testuser";
+        string password = "testpassword";
+        string host = "testhost";
+
+        var mockFile1 = new Mock<ISftpFile>();
+        mockFile1.Setup(f => f.IsDirectory).Returns(false);
+        mockFile1.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile1.Setup(f => f.FullName).Returns("/remote/path/baj05778.txtretrieved");
+        mockFile1.Setup(f => f.Name).Returns("baj05778.txtretrieved");
+
+        var mockFile2 = new Mock<ISftpFile>();
+        mockFile2.Setup(f => f.IsDirectory).Returns(false);
+        mockFile2.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile2.Setup(f => f.FullName).Returns("/remote/path/baj05779.txtretrieved");
+        mockFile2.Setup(f => f.Name).Returns("baj05779.txtretrieved");
+
+        var mockFile3 = new Mock<ISftpFile>();
+        mockFile3.Setup(f => f.IsDirectory).Returns(false);
+        mockFile3.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile3.Setup(f => f.FullName).Returns("/remote/path/baj05780.txt");
+        mockFile3.Setup(f => f.Name).Returns("baj05780.txt");
+
+        var mockFile4 = new Mock<ISftpFile>();
+        mockFile4.Setup(f => f.IsDirectory).Returns(false);
+        mockFile4.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile4.Setup(f => f.FullName).Returns("/remote/path/baj05781.txt");
+        mockFile4.Setup(f => f.Name).Returns("baj05781.txt"); 
+
+        var files = new List<ISftpFile> { mockFile1.Object, mockFile2.Object, mockFile3.Object, mockFile4.Object };
+
+        mockClient.Setup(c => c.ListDirectory(remotePath)).Returns(files);
+
+        var content1 = "content of file1";
+        var content2 = "content of file2";
+        var content3 = "content of file3";
+        var content4 = "content of file4";
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05778.txtretrieved", It.IsAny<Stream>()))
+        .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+        {
+            var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+            writer.Write(content1);
+            writer.Flush();
+        });
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05779.txtretrieved", It.IsAny<Stream>()))
+            .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+            {
+                var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+                writer.Write(content2);
+                writer.Flush();
+            });
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05780.txt", It.IsAny<Stream>()))
+            .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+            {
+                var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+                writer.Write(content3);
+                writer.Flush();
+            });
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05781.txt", It.IsAny<Stream>()))
+            .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+             {
+                 var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+                 writer.Write(content4);
+                 writer.Flush();
+             });
+
+        var client = new ErDataTransClient(remotePath, host, password, username, mockClient.Object);
+
+        // Act
+        var result = client.GetNewFiles(-1);
+
+        // Assert
+        Assert.Equal(4, result.Count);
+
+        var file1 = result[0];
+        Assert.Equal("baj05778.txtretrieved", file1.Item1);
+        file1.Item2.Position = 0;
+        var reader1 = new StreamReader(file1.Item2);
+        Assert.Equal(content1, reader1.ReadToEnd());
+
+        var file2 = result[1];
+        Assert.Equal("baj05779.txtretrieved", file2.Item1);
+        file2.Item2.Position = 0;
+        var reader2 = new StreamReader(file2.Item2);
+        Assert.Equal(content2, reader2.ReadToEnd());
+
+        var file3 = result[2];
+        Assert.Equal("baj05780.txt", file3.Item1);
+        file3.Item2.Position = 0;
+        var reader3 = new StreamReader(file3.Item2);
+        Assert.Equal(content3, reader3.ReadToEnd());
+
+        var file4 = result[3];
+        Assert.Equal("baj05781.txt", file4.Item1);
+        file4.Item2.Position = 0;
+        var reader4 = new StreamReader(file4.Item2);
+        Assert.Equal(content4, reader4.ReadToEnd());
+
+        // Verify rename was called
+        mockClient.Verify(c => c.RenameFile("/remote/path/baj05778.txtretrieved", "/remote/path/baj05778Downloaded.txtretrieved"), Times.Once);
+        mockClient.Verify(c => c.RenameFile("/remote/path/baj05781.txt", "/remote/path/baj05781Downloaded.txt"), Times.Once);
+        mockClient.Verify(c => c.Connect(), Times.Once);
+    }
+
+    [Fact]
+    public void GetNewFiles_Returns1FileFromSftp()
+    {
+        // Arrange
+        var mockClient = new Mock<ISftpClient>();
+        var remotePath = "/remote/path";
+        string username = "testuser";
+        string password = "testpassword";
+        string host = "testhost";
+
+        var mockFile1 = new Mock<ISftpFile>();
+        mockFile1.Setup(f => f.IsDirectory).Returns(false);
+        mockFile1.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile1.Setup(f => f.FullName).Returns("/remote/path/baj05778.txtretrieved");
+        mockFile1.Setup(f => f.Name).Returns("baj05778.txtretrieved");
+
+        var mockFile2 = new Mock<ISftpFile>();
+        mockFile2.Setup(f => f.IsDirectory).Returns(false);
+        mockFile2.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile2.Setup(f => f.FullName).Returns("/remote/path/baj05779.txtretrieved");
+        mockFile2.Setup(f => f.Name).Returns("baj05779.txtretrieved");
+
+        var mockFile3 = new Mock<ISftpFile>();
+        mockFile3.Setup(f => f.IsDirectory).Returns(false);
+        mockFile3.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile3.Setup(f => f.FullName).Returns("/remote/path/baj05780.txt");
+        mockFile3.Setup(f => f.Name).Returns("baj05780.txt");
+
+        var mockFile4 = new Mock<ISftpFile>();
+        mockFile4.Setup(f => f.IsDirectory).Returns(false);
+        mockFile4.Setup(f => f.IsSymbolicLink).Returns(false);
+        mockFile4.Setup(f => f.FullName).Returns("/remote/path/baj05781.txt");
+        mockFile4.Setup(f => f.Name).Returns("baj05781.txt"); 
+
+        var files = new List<ISftpFile> { mockFile1.Object, mockFile2.Object, mockFile3.Object, mockFile4.Object };
+
+        mockClient.Setup(c => c.ListDirectory(remotePath)).Returns(files);
+
+        var content1 = "content of file1";
+        var content2 = "content of file2";
+        var content3 = "content of file3";
+        var content4 = "content of file4";
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05778.txtretrieved", It.IsAny<Stream>()))
+        .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+        {
+            var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+            writer.Write(content1);
+            writer.Flush();
+        });
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05779.txtretrieved", It.IsAny<Stream>()))
+            .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+            {
+                var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+                writer.Write(content2);
+                writer.Flush();
+            });
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05780.txt", It.IsAny<Stream>()))
+            .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+            {
+                var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+                writer.Write(content3);
+                writer.Flush();
+            });
+
+        mockClient.Setup(c => c.DownloadFile("/remote/path/baj05781.txt", It.IsAny<Stream>()))
+            .Callback<string, Stream, Action<ulong>>((path, stream, callback) =>
+             {
+                 var writer = new StreamWriter(stream, Encoding.ASCII, leaveOpen: true);
+                 writer.Write(content4);
+                 writer.Flush();
+             });
+
+        var client = new ErDataTransClient(remotePath, host, password, username, mockClient.Object);
+
+        // Act
+        var result = client.GetNewFiles(5780);
+
+        // Assert
+        Assert.Single(result);
+
+        var file4 = result[0];
+        Assert.Equal("baj05781.txt", file4.Item1);
+        file4.Item2.Position = 0;
+        var reader4 = new StreamReader(file4.Item2);
+        Assert.Equal(content4, reader4.ReadToEnd());
+
+        // Verify rename was called
+        mockClient.Verify(c => c.RenameFile("/remote/path/baj05778.txtretrieved", "/remote/path/baj05778Downloaded.txtretrieved"), Times.Never);
+        mockClient.Verify(c => c.RenameFile("/remote/path/baj05781.txt", "/remote/path/baj05781Downloaded.txt"), Times.Once);
+        mockClient.Verify(c => c.Connect(), Times.Once);
+    }
+
+/* For testing against locally running SFTP server.
+    [Fact]
+    public void GetNewFiles_GetFileFromLocalSFTPServer()
+    {
+        // Arrange
+        var remotePath = "/out/prod/er";
+        string username = "tester";
+        string password = "password";
+        string host = "localhost";
+
+        var client = new ErDataTransClient(username, password, host, remotePath);
+
+        // Act
+        var result1 = client.GetNewFiles(5780);
+
+        var result3 = client.GetNewFiles(-1);
+        var result2 = client.GetNewFiles(-1);
+
+        // Assert
+        Assert.Equal(3, result3.Count);
+        Assert.Empty(result2);
+        Assert.Single(result1);
+    } */
+}
