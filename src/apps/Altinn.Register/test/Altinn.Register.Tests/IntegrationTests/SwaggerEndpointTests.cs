79ff77c40a07fe766759b0e46c7b5f24b35388c6
Aleksander Heintz <alxandr@alxandr.me>
2026-05-13 15:57:25 +0200

    fix tests

--- src/apps/Altinn.Register/test/Altinn.Register.Tests/IntegrationTests/SwaggerEndpointTests.cs
+++ src/apps/Altinn.Register/test/Altinn.Register.Tests/IntegrationTests/SwaggerEndpointTests.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using Altinn.Authorization.ServiceDefaults;
 using Altinn.Common.AccessTokenClient.Services;
 using Altinn.Register.Core.ExternalRoles;
+using Altinn.Register.Core.Location;
 using Altinn.Register.Core.RateLimiting;
 using Altinn.Register.Tests.IntegrationTests.Utils;
 using Altinn.Register.Tests.Mocks;
@@ -29,6 +30,7 @@ public class SwaggerEndpointTests
                 services.AddSingleton<IAccessTokenGenerator, TestAccessTokenGenerator>();
                 services.AddSingleton<IExternalRoleDefinitionPersistence, MockExternalRoleDefinitionPersistence>();
                 services.AddSingleton<IRateLimitProvider, MockRateLimitProvider>();
+                services.AddSingleton<ILocationLookupProvider, MockLocationLookupProvider>();
             });
         });
     }
