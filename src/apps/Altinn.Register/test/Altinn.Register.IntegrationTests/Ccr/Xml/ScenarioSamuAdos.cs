dd01eefa7bf63ba12f7f5801450ef92abcf4f7e6
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2026-05-18 16:58:09 +0200

    27 more test scenarios

---
+++ src/apps/Altinn.Register/test/Altinn.Register.IntegrationTests/Ccr/Xml/ScenarioSamuAdos.cs
@@ -0,0 +1,69 @@
+using System.Diagnostics.CodeAnalysis;
+using Altinn.Register.Contracts;
+using Altinn.Register.Core.Parties;
+using Altinn.Register.Core.Parties.Records;
+using Altinn.Register.Core.UnitOfWork;
+using Altinn.Register.TestUtils.TestData;
+
+namespace Altinn.Register.IntegrationTests.Ccr.Xml;
+
+/// <summary>
+/// SAMU ADOS: Removes the administrative unit - public sector (administrativ-enhet-offentlig-sektor) role of an organization.
+/// </summary>
+public class ScenarioSamuAdos
+    : CcrXmlUpdateTestBase
+{
+    private OrganizationRecord _org = null!;
+    private OrganizationRecord _orgADOS = null!;
+    private PersonRecord _personLede = null!;
+
+    protected override async ValueTask Setup(IUnitOfWork uow, CancellationToken cancellationToken)
+    {
+        _org = await uow.CreateOrg(
+            unitType: "AS",
+            name: "SAMU ADOS TEST AS",
+            cancellationToken: cancellationToken);
+
+        _personLede = await uow.CreatePerson(
+            name: PersonName.Create("Forrige", "Styreleder"),
+            cancellationToken: cancellationToken);
+
+        _orgADOS = await uow.CreateOrg(
+            unitType: "AS",
+            name: "ADOS AS",
+            cancellationToken: cancellationToken);
+
+        await uow.AddRole(ExternalRoleSource.CentralCoordinatingRegister, roleIdentifier: "styreleder", from: _org.PartyUuid.Value, to: _personLede.PartyUuid.Value, cancellationToken);
+        await uow.AddRole(ExternalRoleSource.CentralCoordinatingRegister, roleIdentifier: "administrativ-enhet-offentlig-sektor", from: _org.PartyUuid.Value, to: _orgADOS.PartyUuid.Value, cancellationToken);
+    }
+
+    [StringSyntax(StringSyntaxAttribute.Xml)]
+    protected override string XmlToApply
+        => $$"""
+        <?xml version="1.0" encoding="utf-8"?>
+        <batchAjourholdXML xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="batchAjourholdXML_versjon2_1.xsd">
+          <head avsender="ER" dato="20260504" kjoerenr="05783" mottaker="ALT" type="A" />
+          <enhet organisasjonsnummer="{{_org.OrganizationIdentifier.Value}}" organisasjonsform="ESEK" hovedsakstype="E" undersakstype="EN" foersteOverfoering="N" datoFoedt="20130413" datoSistEndret="20260504">
+            <samendringUtgaar felttype="SAMU">
+                <samendringstype>ADOS</samendringstype>
+            </samendringUtgaar>
+          </enhet>
+          <trai antallEnheter="1" avsender="ER" />
+        </batchAjourholdXML>
+        """;
+
+    protected override async ValueTask Verify(IUnitOfWork uow, CancellationToken cancellationToken)
+    {
+        var parties = uow.GetPartyPersistence();
+        var roles = uow.GetPartyExternalRolePersistence();
+
+        var updatedOrg = await parties.GetOrganizationByIdentifier(_org.OrganizationIdentifier.Value!, PartyFieldIncludes.Party | PartyFieldIncludes.Organization, cancellationToken)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        var roleAssignments = await roles.GetExternalRoleAssignmentsFromParty(partyUuid: _org.PartyUuid.Value, cancellationToken: cancellationToken).ToListAsync(cancellationToken);
+
+        roleAssignments.Count.ShouldBe(1);
+
+        updatedOrg.ShouldNotBeNull();
+    }
+}
