6e870d043258a3074b9254461df5073a8a8d4cb3
Aleksander Heintz <alxandr@alxandr.me>
2026-06-11 14:00:35 +0200

    fix: query perf regression (#917)

--- src/apps/Altinn.Register/test/Altinn.Register.Persistence.Tests/Snapshots/PartyQueryTests.VerifyQuerySnapshot_includes=party,person,org,si,sysuser,org.subunits,user_filterBy=lookupOne(user.name).verified.sql
+++ src/apps/Altinn.Register/test/Altinn.Register.Persistence.Tests/Snapshots/PartyQueryTests.VerifyQuerySnapshot_includes=party,person,org,si,sysuser,org.subunits,user_filterBy=lookupOne(user.name).verified.sql
@@ -12,27 +12,11 @@ filtered_user_ids AS (
     FROM register."user" AS "user"
     WHERE "user".is_active
 ),
-aggregated_user_ids AS (
-    SELECT
-        uuid,
-        max(user_id) FILTER (WHERE is_active) as user_id,
-        array_agg(user_id ORDER BY is_active DESC, user_id DESC) as user_ids
-    FROM filtered_user_ids
-    GROUP BY uuid
-),
 filtered_usernames AS (
     SELECT "username".*
     FROM register."username" AS "username"
     WHERE "username".is_active
        OR "username".username = @username
-),
-aggregated_usernames AS (
-    SELECT
-        uuid,
-        max(username) FILTER (WHERE is_active) as username,
-        array_agg(username ORDER BY is_active DESC, username) as usernames
-    FROM filtered_usernames
-    GROUP BY uuid
 ),
 sub_units AS (
     SELECT
@@ -59,6 +43,24 @@ uuids AS (
         parent_version_id AS sort_first,
         child_uuid AS sort_second
     FROM sub_units
+),
+aggregated_user_ids AS (
+    SELECT
+        uuid,
+        max(user_id) FILTER (WHERE is_active) as user_id,
+        array_agg(user_id ORDER BY is_active DESC, user_id DESC) as user_ids
+    FROM filtered_user_ids
+    INNER JOIN uuids USING (uuid)
+    GROUP BY uuid
+),
+aggregated_usernames AS (
+    SELECT
+        uuid,
+        max(username) FILTER (WHERE is_active) as username,
+        array_agg(username ORDER BY is_active DESC, username) as usernames
+    FROM filtered_usernames
+    INNER JOIN uuids USING (uuid)
+    GROUP BY uuid
 )
 SELECT
     uuids.parent_uuid p_parent_uuid,
