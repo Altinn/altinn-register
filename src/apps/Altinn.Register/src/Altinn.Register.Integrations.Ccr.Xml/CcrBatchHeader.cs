440fc59ba0128c6ca4cbb18a9a1810e3ff56bed2
Aleksander Heintz <alxandr@alxandr.me>
2026-06-15 12:55:07 +0200

    chore: xml processor refactor pt. 1 (#921)

--- src/apps/Altinn.Register/src/Altinn.Register.Integrations.Ccr.Xml/CcrBatchHeader.cs
+++ src/apps/Altinn.Register/src/Altinn.Register.Integrations.Ccr.Xml/CcrBatchHeader.cs
@@ -1,9 +1,12 @@
+using System.Xml;
+
 namespace Altinn.Register.Integrations.Ccr.Xml;
 
 /// <summary>
 /// Represents the header information for a CCR batch, including sender, receiver, date, and batch number details.
 /// </summary>
 internal class CcrBatchHeader
+    : IXmlParsable<CcrBatchHeader>
 {
     /// <summary>
     /// Gets or sets the sender of the message.
@@ -29,4 +32,54 @@ internal class CcrBatchHeader
     /// The type of the batch
     /// </summary>
     public required string Type { get; set; }
+
+    /// <inheritdoc/>
+    public static CcrBatchHeader ParseNode(XmlReader reader)
+    {
+        reader.AssertStartElement("head");
+        reader.AssertEmptyElement();
+
+        string? avsender = null, dato = null, kjoerenr = null, mottaker = null, type = null;
+        while (reader.MoveToNextAttribute())
+        {
+            switch (reader.LocalName)
+            {
+                case "avsender":
+                    avsender = reader.Value;
+                    break;
+
+                case "dato":
+                    dato = reader.Value;
+                    break;
+
+                case "kjoerenr":
+                    kjoerenr = reader.Value;
+                    break;
+
+                case "mottaker":
+                    mottaker = reader.Value;
+                    break;
+
+                case "type":
+                    type = reader.Value;
+                    break;
+
+                default:
+                    // we ignore attributes we don't expect
+                    break;
+            }
+        }
+
+        reader.MoveToElement();
+        reader.Read(); // Consume the <head> element
+
+        return new CcrBatchHeader
+        {
+            Avsender = avsender ?? string.Empty,
+            Dato = dato ?? string.Empty,
+            Kjoerenr = kjoerenr ?? string.Empty,
+            Mottaker = mottaker ?? string.Empty,
+            Type = type ?? string.Empty
+        };
+    }
 }
